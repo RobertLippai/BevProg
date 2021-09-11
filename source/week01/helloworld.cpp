092eabb7ff26b8e4798966dc6bce9e330e7fc22f
Lippai Róbert <robertcody.nl@gmail.com>
2021-09-11 14:07:11 +0200

    Upload first week

---
+++ source/week01/helloworld.cpp
@@ -0,0 +1,13 @@
+#include "std_lib_facilities.h"
+
+int main(){
+
+	cout << "Hello, world!\n";
+
+	return 0; // A program rendben lefutott, ha valami nem lenne jó return -1;
+}
+
+
+//g++ helloworld.cpp -o "helloworld" -std=c++11
+//SSH kulcs
+//Github tároló 
